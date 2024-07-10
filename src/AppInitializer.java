02e25f80dbb0eb61bc631724705289df0844d55a
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-10 21:01:37 +0530

    findAllCustomers done

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,8 +1,12 @@
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class AppInitializer {
     public static void main(String[] args) {
+        //====================================save
         /*Customer c1 = new Customer(
                 "C-001",
                 "Kamal",
@@ -11,8 +15,14 @@ public class AppInitializer {
         if(saveCustomer(c1)){
             System.out.println("Saved..");
         }*/
-        Customer customer = findCustomer("C-001");
-        System.out.println(customer);
+
+        //====================================find
+        /*Customer customer = findCustomer("C-001");
+        System.out.println(customer);*/
+
+        //====================================findAll
+        findAllCustomers().stream().forEach(e-> System.out.println(e));
+
     }
     private static boolean saveCustomer(Customer c){
         try(Session session = HibernateUtil.getSession()){
@@ -26,6 +36,13 @@ public class AppInitializer {
     private static Customer findCustomer(String id){
         try(Session session = HibernateUtil.getSession()){
             return session.find(Customer.class,id);
+        }
+    }
+
+    private static List<Customer> findAllCustomers(){
+        try(Session session = HibernateUtil.getSession()){
+            Query query = session.createQuery("FROM Customer",Customer.class);
+            return query.list();
         }
     }
 }
