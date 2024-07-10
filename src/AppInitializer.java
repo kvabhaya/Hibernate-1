b48732a0108163c4fb251dcf7657670195be0c95
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-10 20:29:22 +0530

    findCustomer done

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -3,10 +3,16 @@ import org.hibernate.Transaction;
 
 public class AppInitializer {
     public static void main(String[] args) {
-        Customer c1 = new Customer("C-001","Kamal","Kandy",25000);
+        /*Customer c1 = new Customer(
+                "C-001",
+                "Kamal",
+                "Kandy",
+                25000);
         if(saveCustomer(c1)){
             System.out.println("Saved..");
-        }
+        }*/
+        Customer customer = findCustomer("C-001");
+        System.out.println(customer);
     }
     private static boolean saveCustomer(Customer c){
         try(Session session = HibernateUtil.getSession()){
@@ -14,6 +20,12 @@ public class AppInitializer {
             session.save(c);
             transaction.commit();
             return true;
+        }
+    }
+
+    private static Customer findCustomer(String id){
+        try(Session session = HibernateUtil.getSession()){
+            return session.find(Customer.class,id);
         }
     }
 }
